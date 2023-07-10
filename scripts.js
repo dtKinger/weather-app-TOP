79b68938cf7ca6ed3663c6a978c39d26ad90f100
Dan King <king.daniel.thomas@gmail.com>
2023-07-10 15:28:13 +0700

    Print unfiltered response object

--- scripts.js
+++ scripts.js
@@ -1,46 +1,104 @@
-console.log("Loading, please wait...")
-
 const searchBtn = document.querySelector('#submit');
 const searchField = document.querySelector('#search');
-let search = '';
+const FORM = document.querySelector('form');
+const OUTPUT = document.querySelector('.output');
+const CURRENT_CITY = document.querySelector('.current-city');
+const newLine = document.createElement('br');
+let lastCity;
 
 searchBtn.addEventListener('click', (e) => {
-e.preventDefault();
-let search = searchField.value;
-fetchWeather(search);
+  e.preventDefault();
+  let search = searchField.value;
+  lastCity = search;
+  fetchWeather(search);
+  FORM.reset(); 
 })
 
 ////////////
 
+// Get user's current city
+
+// Load data based on that.
+
 async function fetchWeather (search) {
-  if (!search){
-    let search = 'Ho Chi Minh';
-  } else {
-      try {
-      const WEATHER_DATA = await fetch(`http://api.weatherapi.com/v1/current.json?key=2218275108374adfbec63623230807&q=${search}`, {
-        mode: 'cors'
-      })
-      let response = await WEATHER_DATA.json()
-      console.log(response)
-      if (error){
-        handleError();  
-      }
+  try {
+    const WEATHER_DATA = await fetch(`http://api.weatherapi.com/v1/current.json?key=2218275108374adfbec63623230807&q=${search}`, {mode: 'cors'})
+    let response = await WEATHER_DATA.json()
+    lastCity = response.location.name;
+    CURRENT_CITY.textContent = `${lastCity}`;
 
-      } catch (error) {
 
-      }   
-    }
+    // OUTPUT.innerHTML = `
+    // <h3>Location information:</h3>
+    // <br><br>
+    // ${response.location.name}
+    // <br><br>
+    // <h3>Current Weather:</h3>
+    // ${response.current}`;
+    
+    OUTPUT.innerHTML = ''; // Dump it before refilling.
+    let breakdown = Object.entries(response)
+    breakdown.forEach((prop) => {
+      let node = document.createElement('p');
+      node.textContent = prop[0];
+      OUTPUT.appendChild(node);
+        let section = document.createElement('ul')
+        node.appendChild(section);
+        let nestedItems = Object.entries(prop[1])
+        nestedItems.forEach((prop, index) => {
+          console.log(prop.hasChildren);
+          let subNode = document.createElement('li')
+          subNode.classList = 'current-weather-item'
+
+          let dataDesc = document.createElement('span')
+          dataDesc.classList = 'data-desc'
+          dataDesc.textContent = prop[0];
+          console.log(dataDesc)
+
+          let value = document.createElement('span')
+          value.classList = 'value'
+          value.textContent = prop[1];
+          console.log(prop[1])
+
+          if (typeof prop[1] === 'object') {
+            
+            let nestedNest = Object.entries(prop[1])
+            let iconPath = nestedNest[1][1]; 
+            let weatherIcon = document.createElement('img')
+            weatherIcon.src = `https:${iconPath}`;
+              console.log(`We found an object at ${prop[1]}`)
+              subNode.textContent = `${dataDesc.textContent}:`  
+              subNode.appendChild(weatherIcon);
+            } else {
+              subNode.textContent = `${dataDesc.textContent}: ${value.textContent}`    
+            }
+          
+
+          section.append(subNode);
+        })
+
+    })
+    
+
+
+    } catch (err) {
+      handleError(err);
+  }   
+}
+
+// Credit Wes Bos
+function handleError(err) {
+  console.log('Ohhhh nooo');
+  console.log(err);
+  // end of credit :')
+  if (response.error.message){
+    console.log(response.error.message);
   }
-  fetchWeather();
+}
 
-  
-  // function handleError(error){
-  //   let tryAgain = prompt(`Oops, we got an error from the server. It said: "${response.error.message}" Want to try again?`)
-  //   if (tryAgain === null){
-  //     // do nothing
-  //   } else {
-  //     city = tryAgain;
-  //     fetchWeather(city);
-  //   }
-  // }
-  // handleError(error)
+// make a safe function with a HOF
+function makeSafe(fn, errorHandler) {
+  return function() {
+    fn().catch(errorHandler);
+  }
+}
